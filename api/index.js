87672ec8ed4fc4a8648ba424dd64f50b4e8a9dc5
Ugochukwu  Nelson Achor <92747656+flamescreatioon@users.noreply.github.com>
2026-04-12 17:56:02 +0100

    Refactor API entry point to directly export server API module

--- api/index.js
+++ api/index.js
@@ -1,9 +1,1 @@
-const dotenv = require('dotenv')
-const serverless = require('serverless-http')
-const app = require('../server/src/app')
-
-dotenv.config()
-
-module.exports = serverless(app, {
-  callbackWaitsForEmptyEventLoop: false,
-})
+module.exports = require('../server/api/index')
